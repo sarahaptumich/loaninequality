64f4e502bc72e362caeaaab00eb2b553fc90816d
sarahaptumich <112976778+sarahaptumich@users.noreply.github.com>
2022-11-08 14:02:39 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+#Import the required Libraries
+import pandas as pd
+import numpy as np
+import altair as alt
+import pyspark
+from pyspark.sql import SparkSession 
+from io import StringIO
+from scipy.stats import ttest_ind
+from causalinference import CausalModel
+import statsmodels.formula.api as smf
+import statsmodels.api as sm
+import streamlit as st
+alt.themes.enable("fivethirtyeight")
+
+# Add a title and intro text
+st.title('Loan Discrimination Exploration')
+st.header("By: Kenny Tang & Sara Haptonstall")
