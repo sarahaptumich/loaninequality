e742af105b3685ab26c776e234e7c8e78339e79d
sarahaptumich <112976778+sarahaptumich@users.noreply.github.com>
2022-11-08 16:10:36 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,5 +19,9 @@ st.write("Recently, we came across an article published by \
          [Reveal news](https://revealnews.org/article/how-we-identified-lending-disparities-in-federal-mortgage-data/), \
          that talks about the presence of discrimination in home mortgage loans in today's society. In their analysis, \
          they were able to determine the likelihood of mortgage denials for different minority groups through the use of \
-         binary logistic regression. The results of their research showed significant discrepancies across 48 different metropolitan areas.")
+         binary logistic regression. The results of their research showed significant discrepancies across 48 different metropolitan areas.<br>\
+         This research was conducted over 5 years ago using 2015 and 2016 mortgage data provided by Home Mortgage Disclosure Act (HMDA). \
+         As years past and America approaches a more culturally diverse society, we would like to know whether discrimination still plays a \
+         significant affect on home mortgage loan approvals for different minority groups. In addition to this, to expand upon the work that \
+         inspired our analysis, we will also observe discrepancies in interest rates for different minority groups.")
 
