bb89b7dd7d9b4ca6e9769407de6a9ebbfa377fe9
sarahaptumich <112976778+sarahaptumich@users.noreply.github.com>
2022-11-08 16:07:46 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,4 +15,9 @@ alt.themes.enable("fivethirtyeight")
 # Add a title and intro text
 st.title('Loan Discrimination Exploration')
 st.subheader("By: Kenny Tang & Sara Haptonstall")
-st.write("Recently, we came across an article published by [Reveal News](https://revealnews.org/article/how-we-identified-lending-disparities-in-federal-mortgage-data/), that talks about the presence of discrimination in home mortgage loans in today's society. In their analysis, they were able to determine the likelihood of mortgage denials for different minority groups through the use of binary logistic regression. The results of their research showed significant discrepancies across 48 different metropolitan areas.")
+st.write("Recently, we came across an article published by \
+         [Reveal news](https://revealnews.org/article/how-we-identified-lending-disparities-in-federal-mortgage-data/), \
+         that talks about the presence of discrimination in home mortgage loans in today's society. In their analysis, \
+         they were able to determine the likelihood of mortgage denials for different minority groups through the use of \
+         binary logistic regression. The results of their research showed significant discrepancies across 48 different metropolitan areas.")
+
