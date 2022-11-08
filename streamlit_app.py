ce9bea70b37f7ed28eb8b9e7b4e9f425553034f7
sarahaptumich <112976778+sarahaptumich@users.noreply.github.com>
2022-11-08 16:31:40 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,12 +37,11 @@ import numpy as np
 import altair as alt
 import pyspark
 from pyspark.sql import SparkSession 
-from io import StringIO
 from scipy.stats import ttest_ind
 from causalinference import CausalModel
 import statsmodels.formula.api as smf
 import statsmodels.api as sm
-import streamlit as st
+#theme
 alt.themes.enable("fivethirtyeight")'''
 
 st.code(libraries, language='python')
